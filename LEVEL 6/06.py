58cc786fa2be782d641402b0b75794622053030a
NIDHI <nidhibaranawal456@gmail.com>
2025-10-30 21:57:34 +0530

    code

---
+++ LEVEL 6/06.py
@@ -0,0 +1,13 @@
+a = int(input("Enter marks obtained by the student:"))
+if (a>=90 and a<=100):
+    print("Excellent")
+elif(a>=80 and a<90):
+    print("Grade A")
+elif(a>=70 and a<80):
+    print("Grade B")
+elif(a>=60 and a<70):
+    print("Grade C")
+elif(a>=50 and a<60):
+    print("Grade D")
+else:
+    print("Grade F")
---
+++ LEVEL 6/07.py
@@ -0,0 +1,7 @@
+a = input("Enter post:")
+
+if ("Nidhi".lower() in a.lower()):
+    print("Post is about Nidhi")
+    
+else:
+    print("Post is not about Nidhi")
